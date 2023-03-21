517b8881f7a9ab21283d388dcd24e0c3b9198ef8
Irina <irinakhr@mail.ru>
2023-03-21 21:42:55 +0100

    added bootstrap

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,7 @@
   <div class="home">
     <NavBar/>
     <ServicesSection/>
+    <TestimonialSection/>
     <FooterSection/>
   </div>
 </template>
@@ -11,12 +12,14 @@
 import NavBar from '@/components/NavBar.vue';
 import ServicesSection from '@/components/ServicesSection.vue';
 import FooterSection from '@/components/FooterSection.vue';
+import TestimonialSection from '@/components/TestimonialSection.vue';
 
 export default {
   name: 'HomeView',
   components: {
     NavBar,
     ServicesSection,
+    TestimonialSection,
     FooterSection,
   },
 };
